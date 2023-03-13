cab3d5335c12a95fc08488e78ef01dbf8901bfa8
cosine0 <ksitht@gmail.com>
2023-03-13 20:19:38 +0900

    add feature to turn off strict hierarchical order

--- parallel_diff_tree.py
+++ parallel_diff_tree.py
@@ -1,9 +1,12 @@
 """ compare two directories in parallel (metadata is ignored) """
 
 import functools
+import os
+import shutil
 import sys
 import threading
 from pathlib import Path
+from typing import Set, Union
 
 from parallel_traversal import parallel_recursive_apply
 
@@ -12,41 +15,47 @@ print_lock = threading.Lock()
 
 def diff_dir(src: Path, src_root: Path, dest_root: Path) -> None:
     """ compare src dir to the corresponding location in dest_root """
+    terminal_width = shutil.get_terminal_size().columns
     dest = dest_root / src.relative_to(src_root)
     if not dest.exists():
         with print_lock:
-            print(f"\rDELETED Dir: {src} -> x")
+            print(f"\rDELETED Dir: {src} -> x".ljust(terminal_width))
     elif not dest.is_dir():
         with print_lock:
-            print(f"\rPROPERTY CHANGED [DIR]{src} -> [FILE]{dest}")
+            print(f"\rPROPERTY CHANGED [DIR]{src} -> [FILE]{dest}".ljust(
+                terminal_width))
     else:
         # report created direct children
-        src_children = set(src.iterdir())
-        dest_children = set(dest.iterdir())
+        src_children: Set[Union[str, bytes, os.PathLike]] = set(os.listdir(src))
+        dest_children: Set[Union[str, bytes, os.PathLike]] = \
+            set(os.listdir(dest))
         for child in dest_children - src_children:
-            if child.is_dir():
+            if (dest / child).is_dir():
                 with print_lock:
-                    print(f"\rCREATED Dir: x -> {child}")
+                    print(f"\rCREATED Dir (maybe also children, "
+                          f"not checked): x -> {child}".ljust(terminal_width))
             else:
                 with print_lock:
-                    print(f"\rCREATED File: x -> {child}")
+                    print(f"\rCREATED File: x -> {child}".ljust(terminal_width))
 
 
 def diff_file(src: Path, src_root: Path, dest_root: Path) -> None:
     """ compare src file to the corresponding location in dest_root """
+    terminal_width = shutil.get_terminal_size().columns
     dest = dest_root / src.relative_to(src_root)
     if not dest.exists():
         with print_lock:
-            print(f"\rDELETED File: {src} -> x")
+            print(f"\rDELETED File: {src} -> x".ljust(terminal_width))
     elif not dest.is_file():
         with print_lock:
-            print(f"\rPROPERTY CHANGED [FILE]{src} -> [DIR]{dest}")
+            print(f"\rPROPERTY CHANGED [FILE] {src} -> [DIR] {dest}".ljust(
+                terminal_width))
     else:
         # compare size
         if src.stat().st_size != dest.stat().st_size:
             with print_lock:
-                print(f"\rSIZE CHANGED File: {src}[{src.stat().st_size}] "
-                      f"-> {dest}[{dest.stat().st_size}]")
+                print(f"\rSIZE CHANGED File: [{src.stat().st_size}] {src} -> "
+                      f"[{dest.stat().st_size}] {dest}".ljust(terminal_width))
             return
         # compare content
         with src.open("rb") as src_file, dest.open("rb") as dest_file:
@@ -55,7 +64,8 @@ def diff_file(src: Path, src_root: Path, dest_root: Path) -> None:
                 dest_data = dest_file.read(128 * 1024)
                 if src_data != dest_data:
                     with print_lock:
-                        print(f"\rCONTENT CHANGED File: {src} -> {dest}")
+                        print(f"\rCONTENT CHANGED File: {src} -> {dest}".ljust(
+                            terminal_width))
                     break
                 if not src_data:
                     break
@@ -81,6 +91,7 @@ def parallel_diff_tree(source_path: str, dest_path: str,
         file_func=functools.partial(diff_file, dest_root=dest),  # type: ignore
         num_max_threads=num_max_threads,
         pre_order=False,
+        strict_hierarchical_order=True,
         print_lock=print_lock
     )
 
